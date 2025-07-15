743a6552631329c1b2479dfd2eb72ee04ff6f9d1
gayat <gayat@DESKTOP-R06THM1>
2025-07-15 13:14:48 -0700

    Initial project setup with pom.xml and BaseTest class

---
+++ Merchandise/src/main/java/EcommerceSite/Merchandise/App.java
@@ -0,0 +1,13 @@
+package EcommerceSite.Merchandise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Merchandise/src/main/java/Merchandise/AbstractComponents/Abstract.java
@@ -0,0 +1,5 @@
+package Merchandise.AbstractComponents;
+
+public class Abstract {
+
+}
